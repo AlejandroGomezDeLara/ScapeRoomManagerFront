47beae8e6c1ca66bb1be7024dae700e7f07c9dec
Alejandro <alejandro.gomez711@gmail.com>
2022-12-16 09:44:44 +0100

    Home completamente responsive

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000/api/auth/',
+  apiUrl: 'http://192.168.1.141:80/api/auth/',
   googleApiKey:'AIzaSyAp3sD3cJc6L3Nbrul7-GNTOM74l_NS2Qw',
   appName:'Funly'
 };
